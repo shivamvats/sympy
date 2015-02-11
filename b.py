b1473267656816cbdbaed75f43727a89bf0caab8
Ondřej Čertík <ondrej.certik@gmail.com>
2015-02-10 17:21:49 -0700

    Add rs_* benchmark

---
+++ b.py
@@ -0,0 +1,19 @@
+print "import"
+from timeit import default_timer as clock
+from sympy.polys.domains import ZZ, QQ
+from sympy.polys.rings import ring
+from sympy.polys.ring_series import rs_mul, rs_exp, rs_log
+print "done"
+
+R, x = ring('x', QQ)
+
+n = 200
+a = rs_log(1+x, x, n)
+b = rs_exp(x, x, n)
+print "1"
+t1 = clock()
+s = rs_mul(a, b, x, n)
+t2 = clock()
+print "2"
+#print s
+print t2-t1
