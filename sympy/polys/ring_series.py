7b4dd8077192204759ead3fad5584b82f0c62720
Ondřej Čertík <ondrej.certik@gmail.com>
2015-02-11 18:10:37 -0700

    Revert the changes

--- sympy/polys/ring_series.py
+++ sympy/polys/ring_series.py
@@ -1,6 +1,5 @@
 """Power series manipulating functions acting on polys.ring.PolyElement()"""
 
-from timeit import default_timer as clock
 from sympy.polys.domains import QQ
 from sympy.polys.rings import PolyElement
 from sympy.polys.monomials import monomial_min, monomial_mul
@@ -100,7 +99,7 @@ def rs_mul(p1, p2, x, prec):
     """
 
     ring = p1.ring
-    p = {}
+    p = ring.zero
     if ring.__class__ != p2.ring.__class__ or ring != p2.ring:
         raise ValueError('p1 and p2 must have the same ring')
     iv = ring.gens.index(x)
@@ -108,24 +107,18 @@ def rs_mul(p1, p2, x, prec):
         raise ValueError('p1 and p2 must have the same ring')
     if ring == p2.ring:
         get = p.get
-        items1 = list(p1.items())
-        items1.sort(key=lambda e: e[0][iv])
         items2 = list(p2.items())
         items2.sort(key=lambda e: e[0][iv])
-        items1 = [(a[0][0], a[1]) for a in items1]
-        items2 = [(a[0][0], a[1]) for a in items2]
         if ring.ngens == 1:
-            t1 = clock()
-            for exp1, v1 in items1:
+            for exp1, v1 in p1.items():
                 for exp2, v2 in items2:
-                    exp = exp1 + exp2
+                    exp = exp1[0] + exp2[0]
                     if exp < prec:
+                        exp = (exp, )
                         p[exp] = get(exp, 0) + v1*v2
                     else:
                         break
-            t2 = clock()
         else:
-            assert False
             monomial_mul = ring.monomial_mul
             for exp1, v1 in p1.items():
                 for exp2, v2 in items2:
@@ -135,10 +128,6 @@ def rs_mul(p1, p2, x, prec):
                     else:
                         break
 
-    pold = p
-    p = ring.zero
-    for a in pold:
-        p[(a, )] = pold[a]
     p.strip_zero()
     return p
 
